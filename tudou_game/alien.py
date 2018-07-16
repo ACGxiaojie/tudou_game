bd5caa0444ea3fb2e9cc7af201979ff2819c36f7
ACGxiaojie <39219756+ACGxiaojie@users.noreply.github.com>
2018-07-16 15:51:19 +0800

    Update alien.py

--- tudou_game/alien.py
+++ tudou_game/alien.py
@@ -6,7 +6,7 @@ class Alien(Sprite):
 		self.screen = screen
 		self.ai_settings = ai_settings
 		
-		self.image = pygame.image.load(r'I:\python_game\images\alien.bmp')
+		self.image = pygame.image.load(r'I:\tudou_game\images\alien.bmp')
 		self.rect = self.image.get_rect()
 		self.rect.x = self.rect.width
 		self.rect.y = self.rect.height
